027dbad6d51e19a9a1e1d37fbf77befbe11d9c9f
david_venegas <venegasdavidm@gmail.com>
2021-12-17 17:35:14 -0500

    fixed button for lists

--- src/3-Recipes/Recipes.js
+++ src/3-Recipes/Recipes.js
@@ -30,6 +30,7 @@ function Recipes() {
                 const snapshot = await getDocs(q)
                 const results = snapshot.docs.map((doc) => ({ ...doc.data(), id:doc.id }))
                 setSavedGroceries(results)
+                console.log(savedGroceries);
         }
         queryRecipes()
         queryGroceries()
@@ -100,11 +101,11 @@ function Recipes() {
                             <p onClick={handleDeleteAll} id="recipes-remove-all">Remove all items</p>
                         </div>
                         <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                            <button className="recipe-flip-button-front" onClick={handleFlip}><span className="recipe-flip-front-plus">+</span><p className="recipe-flip-front-title">Create New List</p></button>
+                            <button style={savedRecipes.length > 0 ? {pointerEvents: "all"} : {pointerEvents: "none", opacity: ".5" }} className="recipe-flip-button-front" onClick={handleFlip}><span className="recipe-flip-front-plus">+</span><p className="recipe-flip-front-title">Create New List</p></button>
                             <button className="recipe-flip-button-back">
                                     <img className="recipe-flip-text-cancel" onClick={handleFlip} src={RedXIcon} alt='thin_x'/>
                                     <form className="recipe-flip-text-form" onSubmit={handleSubmit}>
-                                        <input className="recipe-flip-text-input" id="focusText" type="text" placeholder='Name your list...' onChange={handleChange} required/>
+                                        <input className="recipe-flip-text-input" maxLength="12" id="focusText" type="text" placeholder='Name your list...' onChange={handleChange} required/>
                                         <button className="recipe-flip-text-submit-wrapper" type="submit">
                                             <img className="recipe-flip-text-submit" src={RightArrow} alt="right_arrow" />
                                         </button>
@@ -129,14 +130,14 @@ function Recipes() {
                 </div>
                 <div className="recipes-box-2">
                     <div className="recipes-shopping-list-wrapper">
-                        <h1 className="recipes-shopping-lists-title">Shopping Lists (<span>5</span>)</h1>
+                        <h1 className="recipes-shopping-lists-title">Shopping Lists (<span>{savedGroceries.length}</span>)</h1>
                         <div className="recipes-shopping-lists-content-seperator"></div>
                         <div className="recipes-shopping-lists-container">
                             {savedGroceries.map((list) => (
                                 <div id='recipes-list-item-wrapper'>
                                     <img id='recipes-list-item-delete' onClick={() => deleteGroceryList(list.id)} src={TrashIcon} alt="trash_icon" />
                                     <Link id='recipes-list-item' to='/groceries' state={{list : list}}>
-                                        <h1>{list.listName}</h1>
+                                        <h1 style={list.listName.length > 10 ? {fontSize: '1.85rem'} : {fontSize: '2.25rem'}}>{list.listName}</h1>
                                     </Link>
                                 </div>
                             ))}
